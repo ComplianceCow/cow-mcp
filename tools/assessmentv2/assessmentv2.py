004e4eb171a4e4e076da798feb540f20c5329d87
raja-ccow <raja.s@compliancecow.com>
2026-04-08 09:51:48 +0530

    fetch last run date tool added to policy mcp (#252)
    Co-authored-by: Raja S <raja.s@continube.com>

--- tools/assessmentv2/assessmentv2.py
+++ tools/assessmentv2/assessmentv2.py
@@ -11,7 +11,7 @@ from fastmcp import Context
 @mcp.tool()
 async def get_default_ccf_assessment(ctx: Context | None = None) -> vo.AssessmentVO:
     """
-        Get the default ccf assessment from the domain preferences.
+        Get the default ccf assessment from CCow.
     """
     try:
         logger.info("get_default_ccf_assessment: \n")
@@ -28,3 +28,28 @@ async def get_default_ccf_assessment(ctx: Context | None = None) -> vo.Assessmen
     except Exception as e:
         logger.error("get_default_ccf_assessment error: {}\n".format(e))
         return vo.AssessmentVO(error="Facing internal error")
+    
+
+@mcp.tool()
+async def get_ccf_control_last_run_date(ccfControlIDs: List[str], ctx: Context | None = None) -> vo.ControlsLastRunDate:
+    """
+        Get the ccf assessment controls last run date. 
+        
+        input:
+        ccfControlIDs: id (GUID) field of leaf controls in ccf assessment
+    """
+    try:
+        logger.info("get_ccf_control_last_run_date: {} \n".format(ccfControlIDs))
+
+        output=await utils.make_API_call_to_CCow({"ControlIDs": ccfControlIDs},constants.URL_PLAN_INSTANCE_CONTROLS+"/fetch-latest-run-date", ctx)
+        
+        if isinstance(output, str) or  "error" in output:
+            logger.error("get_ccf_control_last_run_date error: {}\n".format(output))
+            return vo.ControlsLastRunDate(error="Facing internal error")
+    
+        assessment=vo.ControlsLastRunDate(**output)
+        logger.debug("last run date: {}\n".format(output))
+        return assessment
+    except Exception as e:
+        logger.error("get_ccf_control_last_run_date error: {}\n".format(e))
+        return vo.ControlsLastRunDate(error="Facing internal error")
